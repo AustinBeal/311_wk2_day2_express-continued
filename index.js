c78113305996b973612db1b24244530d76cfabcb
AustinBeal <austinbeal89@gmail.com>
2021-01-11 16:44:21 -0600

    finished homework

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+app.use(require("./routes/comments.js"));
+
+app.use(require("./routes/contacts.js"));
+
+app.use(require("./routes/products.js"));
+
+app.use(require("./routes/vehicles.js"));
+
+
+
+
 
 const port = process.env.PORT || 4001;
 
